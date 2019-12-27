b4afa7f8304db0677699759907fd8e5dc659f5b4
zhangshuai <zhangshuai@mac.local>
2019-12-27 17:42:21 +0900

    Optimize the code

--- lib/main.dart
+++ lib/main.dart
@@ -6,83 +6,70 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
+  Widget build(BuildContext context) => MaterialApp(
       title: 'Startup Name Generator',
-      theme: new ThemeData(
-        primaryColor: Colors.lightBlueAccent,
-      ),
-      home: new RandomWords(),
+      theme: ThemeData(primaryColor: Colors.lightBlueAccent),
+      home: RandomWords(),
     );
-  }
 }
 
 class RandomWords extends StatefulWidget {
   @override
-  createState() => new RandomWordsState();
+  createState() => RandomWordsState();
 }
 
 class RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = List<WordPair>.from(generateWordPairs().take(30));
 
-  final Set<WordPair> _saved = new Set<WordPair>();
+  final Set<WordPair> _saved = Set<WordPair>();
 
   final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
 
   @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Startup Name Generator'),
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
         actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
+          IconButton(
+              icon: Icon(Icons.list),
+              onPressed: _pushSaved
+          ),
         ],
       ),
       body: _buildSuggestions(),
     );
-  }
 
   void _pushSaved() {
     Navigator.of(context).push(
-      new MaterialPageRoute(
+      MaterialPageRoute(
         builder: (context) {
-          final tiles = _saved.map(
-            (pair) {
-              return new ListTile(
-                title: new Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
+          final tiles = _saved.map((pair) => ListTile(
+            title: Text(
+              pair.asPascalCase,
+              style: _biggerFont,
+            ),
+          ));
           final divided = ListTile.divideTiles(
             context: context,
             tiles: tiles,
           ).toList();
-          return new Scaffold(
-            appBar: new AppBar(
-              title: new Text('Saved Suggestions'),
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
             ),
-            body: new ListView(children: divided),
+            body: ListView(children: divided),
           );
         },
       ),
     );
   }
 
-  Widget _buildSuggestions() {
-    return new ListView.builder(
+  Widget _buildSuggestions() => ListView.builder(
         itemCount: _suggestions.length * 2,
         padding: const EdgeInsets.all(16.0),
-        // 对于每个建议的单词对都会调用一次itemBuilder，然后将单词对添加到ListTile行中
-        // 在偶数行，该函数会为单词对添加一个ListTile row.
-        // 在奇数行，该函数会添加一个分割线widget，来分隔相邻的词对。
-        // 注意，在小屏幕上，分割线看起来可能比较吃力。
         itemBuilder: (context, i) {
           // 在每一列之前，添加一个1像素高的分隔线widget
-          if (i.isOdd) return new Divider();
-
+          if (i.isOdd) return Divider();
           // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
           // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
           final index = i ~/ 2;
@@ -93,17 +80,16 @@ class RandomWordsState extends State<RandomWords> {
 //          }
           return _buildRow(_suggestions[index]);
         });
-  }
 
   Widget _buildRow(WordPair pair) {
     final bool alreadySaved = _saved.contains(pair);
 
-    return new ListTile(
-      title: new Text(
+    return ListTile(
+      title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
-      trailing: new Icon(
+      trailing: Icon(
         alreadySaved ? Icons.favorite : Icons.favorite_border,
         color: alreadySaved ? Colors.red : null,
       ),
