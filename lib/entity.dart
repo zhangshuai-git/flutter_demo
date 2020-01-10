92e71c658c69b028f49d4c314e847a23d6bd1fc5
zhangshuai <zhangshuai@mac.local>
2020-01-10 19:30:07 +0900

    Add Word

---
+++ lib/entity.dart
@@ -0,0 +1,10 @@
+import 'package:english_words/english_words.dart';
+import 'package:rxdart/rxdart.dart';
+
+class Word {
+
+  WordPair wordPair;
+  BehaviorSubject<bool> isFavorite;
+
+  Word(this.wordPair, this.isFavorite);
+}
--- lib/word_bloc.dart
+++ lib/word_bloc.dart
@@ -1,10 +1,16 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_demo1/entity.dart';
+import 'package:rxdart/rxdart.dart';
 
 class WordBloc {
-  final List<WordPair> suggestions = List<WordPair>.from(generateWordPairs().take(30));
+  final List<Word> suggestions = List<Word>.from(generateWordPairs().take(30).map((wordPair) => Word(wordPair, BehaviorSubject.seeded(false))));
 
   final Set<WordPair> saved = Set<WordPair>();
 
   final TextStyle biggerFont = const TextStyle(fontSize: 18.0);
+
+  void dispose() {
+    suggestions.forEach((it) => it.isFavorite.close());
+  }
 }
