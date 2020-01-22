a2da24c220eb5c020f9dc2ecda76209c17fa23b2
zhangshuai <zhangshuai@mac.local>
2020-01-22 18:33:54 +0900

    Add database service

--- lib/service/database_service.dart
+++ lib/service/database_service.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:flutter_demo1/model/entity.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -18,12 +19,12 @@ class DatabaseService {
     if (_database == null) {
       Directory directory = await getApplicationDocumentsDirectory();
       String path = directory.path + 'repositories.sqlite';
-      _database = await openDatabase(path, version: 1, onCreate: _creatTable);
+      _database = await openDatabase(path, version: 1, onCreate: _createTable);
     }
     return _database;
   }
 
-  void _creatTable(Database db, int newVersion) async {
+  void _createTable(Database db, int newVersion) async {
     await db.execute("""
       CREATE TABLE IF NOT EXISTS 'repository' (\
       'id' INTEGER PRIMARY KEY AUTOINCREMENT  NOT NULL ,\
@@ -46,4 +47,49 @@ class DatabaseService {
     """);
   }
 
+  Future<void> add(Repository repository) async {
+    print("add ${repository.name}");
+    final db = await this.database;
+    db.execute("INSERT INTO repository(id, own_id, name, full_name, html_url, description, comment)VALUES(?,?,?,?,?,?,?)",
+      [repository.id, repository.owner.id, repository.name, repository.fullName, repository.htmlUrl, repository.desp, repository.comment]);
+    db.execute("INSERT INTO repository_owner(id, login, url, avatar_url)VALUES(?,?,?,?)",
+      [repository.owner.id, repository.owner.login, repository.owner.url, repository.owner.avatarUrl]);
+  }
+
+  Future<void> delete(Repository repository) async {
+    print("delete ${repository.name}");
+    final db = await this.database;
+    db.execute("DELETE FROM repository WHERE id = ?", [repository.id]);
+  }
+
+  Future<void> update(Repository repository) async {
+    print("update ${repository.name}");
+    final db = await this.database;
+    db.execute("UPDATE 'repository' SET name = ?  WHERE id = ? ", [repository.name, repository.id]);
+    db.execute("UPDATE 'repository' SET full_name = ?  WHERE id = ? ", [repository.fullName, repository.id]);
+    db.execute("UPDATE 'repository' SET html_url = ?  WHERE id = ? ", [repository.htmlUrl, repository.id]);
+    db.execute("UPDATE 'repository' SET description = ?  WHERE id = ? ", [repository.desp,   repository.id]);
+    db.execute("UPDATE 'repository' SET comment = ?  WHERE id = ? ", [repository.comment, repository.id]);
+  }
+
+  Future<List<Repository>> getAllRepository() async {
+    print("getAllRepository");
+    final db = await this.database;
+    final res = db.rawQuery("SELECT * FROM repository ").then((it) => it.map((it) => Repository.fromJson(it)));
+    return res;
+  }
+
+  Future<Repository> getRepository(int id) async {
+    print("getRepository $id");
+    final db = await this.database;
+    final res = db.rawQuery("SELECT * FROM repository where id = ? ", [id]).then((it) => Repository.fromJson(it.last));
+    return res;
+  }
+
+  Future<RepositoryOwner> getRepositoryOwner(int id) async {
+    print("getRepositoryOwner $id");
+    final db = await this.database;
+    final res = db.rawQuery("SELECT * FROM repository_owner where id = ? ", [id]).then((it) => RepositoryOwner.fromJson(it.last));
+    return res;
+  }
 }
