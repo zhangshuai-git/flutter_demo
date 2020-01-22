a21934ddecad34a88b9a07185ea8c3bf3404a0dc
zhangshuai <zhangshuai@mac.local>
2020-01-22 17:41:02 +0900

    Add database service

--- lib/service/database_service.dart
+++ lib/service/database_service.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
 
 class DatabaseService {
   static DatabaseService _instance;
@@ -10,5 +13,37 @@ class DatabaseService {
   }
   factory DatabaseService.getInstance() => _getInstance();
 
+  Database _database;
+  Future<Database> get database async {
+    if (_database == null) {
+      Directory directory = await getApplicationDocumentsDirectory();
+      String path = directory.path + 'repositories.sqlite';
+      _database = await openDatabase(path, version: 1, onCreate: _creatTable);
+    }
+    return _database;
+  }
+
+  void _creatTable(Database db, int newVersion) async {
+    await db.execute("""
+      CREATE TABLE IF NOT EXISTS 'repository' (\
+      'id' INTEGER PRIMARY KEY AUTOINCREMENT  NOT NULL ,\
+      'own_id' INT,\
+      'name' VARCHAR(255),\
+      'full_name' VARCHAR(255),\
+      'html_url' VARCHAR(255),\
+      'description' VARCHAR(255),\
+      'comment' VARCHAR(255)\
+      )
+    """);
+
+    await db.execute("""
+      CREATE TABLE IF NOT EXISTS 'repository_owner' (\
+      'id' INTEGER PRIMARY KEY AUTOINCREMENT  NOT NULL ,\
+      'login' VARCHAR(255),\
+      'url' VARCHAR(255),\
+      'avatar_url' VARCHAR(255)\
+      )
+    """);
+  }
 
 }
